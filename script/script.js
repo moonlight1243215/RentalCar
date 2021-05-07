a645121e7d21f9c2a40ab3634b9668fd6d6c6f28
moonlighe1243215 <krzys.szywacz@gmail.com>
2021-05-07 14:52:42 +0200

    failed version

--- script/script.js
+++ script/script.js
@@ -8,6 +8,10 @@ const car = [
       seatCount: 2,
       horsePower: 750,
       drive: "4x4",
+      valueIsRented: true,
+      rented: "",
+      availableRentDays: [1, 2, 3, 5, 10],
+      rentPrice: 2121
     },
     {
       carPhoto: "assets/photo/nissan.png",
@@ -18,6 +22,10 @@ const car = [
       seatCount: 2,
       horsePower: 550,
       drive: "4x4",
+      valueIsRented: true,
+      rented: "",
+      availableRentDays: [2, 4, 6, 8, 10],
+      rentPrice: 3132
     },
     {
       carPhoto: "assets/photo/bmw.png",
@@ -28,17 +36,31 @@ const car = [
       seatCount: 5,
       horsePower: 625,
       drive: "4x4",
+      valueIsRented: true,
+      rented: "",
+      availableRentDays: [1, 2, 5, 10, 15],
+      rentPrice: 1200
     },
   ];
   
   const main = document.getElementById("container");
 
+
   const carItem = car.forEach(item => {
     const createLi = document.createElement("div");
     createLi.className = "box-car";
 
+    const Cars = () => {
+      if(item.valueIsRented === true) {
+        document.innerHTML = "wyporzyczone"
+      }
+      else {
+        document.innerHTML = "w magazynie"
+      }
+    };
+
     const contentLi = document.createElement("div")
-    contentLi.className = "content-box-car"
+    contentLi.className = "content-box-car"  
 
     contentLi.innerHTML = `
     <img src="${item.carPhoto}" width="400px" height="225px" class="car-photo"/>
@@ -49,16 +71,10 @@ const car = [
     <p class="text">Napęd: ${item.drive}</p>
     <p class="text">Moc silnika: ${item.horsePower} KM</p>
     <p class="text">Liczba miejsc siedzących: ${item.seatCount}</p>
+    <p class="text">Cena wyporzyczenia za dzień: ${item.rentPrice}zł</p>
+    <p class="text">Auto jest: ${Cars()}</p>
     <button class="button">WYNAJMIJ</button>
   `
   createLi.appendChild(contentLi)
   main.appendChild(createLi)
   })
-
-  
-
-
-
-
-
-
